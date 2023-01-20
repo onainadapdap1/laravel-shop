895dacc48e8cc3f78e7293dd56929093edf4c563
Onai Nadapdap <onainadapdap1@gmail.com>
2023-01-20 21:36:25 +0700

    make a navbar for front end

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.frontend')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    <h1 class="mt-5">Hello of funda web it</h1>
+@endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <title> @yield('title') | fabcart</title>
     {{-- font awesome --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
@@ -18,7 +18,15 @@
 </head>
 
 <body>
-    
+
+    @include('layouts.inc.front-navbar')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('layouts.inc.front-footer')
+
     <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/popper.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
